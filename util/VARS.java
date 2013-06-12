c29b3d5066a708021dc1cebf91e22f7946fe3892
SmartScripts <youngmoneyfyi@yahoo.com>
2013-06-12 02:16:13 -0400

    Create VARS.java

---
+++ util/VARS.java
@@ -0,0 +1,14 @@
+package FisherCooker.Util;
+
+
+public class VARS {
+  public static final int LOGS_ID = 1511;
+	public static final int BONFIRE_ID = 70755;
+	
+	public static final int [] COOKED_FISH_IDS = {333 ,329, 70};
+	public static final int [] FISH_IDS = {335,331};
+	public static final int [] TREE_ID = {38785, 38783, 38760} ;
+	
+	public static boolean fireLit = false;
+	public static boolean cook = true;
+}
