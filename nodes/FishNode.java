44398cf7b83d6f4f208b954382fc3828a68ea694
SmartScripts <youngmoneyfyi@yahoo.com>
2013-06-12 19:01:26 -0300

    Update FishNode.java

--- nodes/FishNode.java
+++ nodes/FishNode.java
@@ -2,15 +2,12 @@ package FisherCooker.nodes;
 
 import org.powerbot.core.script.job.Task;
 import org.powerbot.core.script.job.state.Node;
-import org.powerbot.game.api.methods.Walking;
 import org.powerbot.game.api.methods.Widgets;
 import org.powerbot.game.api.methods.interactive.NPCs;
 import org.powerbot.game.api.methods.interactive.Players;
 import org.powerbot.game.api.methods.node.Menu;
 import org.powerbot.game.api.methods.tab.Inventory;
-import org.powerbot.game.api.methods.widget.Camera;
 import org.powerbot.game.api.util.Timer;
-import org.powerbot.game.api.wrappers.Locatable;
 import org.powerbot.game.api.wrappers.interactive.NPC;
 import org.powerbot.game.api.wrappers.widget.WidgetChild;
 
@@ -18,8 +15,8 @@ import FisherCooker.util.*;
 
 
 public class FishNode extends Node{
-  
-	private static final int FISHING_SPOT_ID = 328;
+	
+	private  final int FISHING_SPOT_ID = 328;
 	
 	private static final WidgetChild EAT_TROUT_ACTIONBAR = Widgets.get(640, 70);
 	private static final WidgetChild EAT_SALMON_ACTIONBAR = Widgets.get(640, 75);
@@ -42,47 +39,41 @@ public class FishNode extends Node{
 			Widgets.get(1218, 76).click(true);
 		}
 		if(fishingSpot != null){
-			getOnScreen(fishingSpot);
+			Methods.getOnScreen(fishingSpot);
 			if(Players.getLocal().getInteracting() == null && fishingSpot.interact("Lure")){
-				while(Players.getLocal().getInteracting() != null && Inventory.contains(VARS.COOKED_FISH_IDS) ){
+				while(Players.getLocal().getInteracting() != null && Inventory.contains(VARS.COOKED_FISH_IDS)){
 					dropFish();
 					Task.sleep(50, 75);
 				}
 			}
 		}
 	}
-	private void getOnScreen(Locatable l){
-		if(!l.getLocation().isOnScreen())
-			Camera.turnTo(l);
-		if(!l.getLocation().isOnScreen())
-			Walking.walk(l);
-			
-	}
 
-	
 	private boolean dropFish(){
 		while(Inventory.contains(VARS.COOKED_FISH_IDS[0])){
-			if(EAT_TROUT_ACTIONBAR.click(false)){
-				Timer t = new Timer(1000);
+			if(EAT_TROUT_ACTIONBAR != null && EAT_TROUT_ACTIONBAR.click(false)){
+				Timer t = new Timer(2000);
 				while(!Menu.isOpen() && t.isRunning()){
-					Task.sleep(200,300);
+					Task.sleep(50,75);
 				}
-				Menu.clickIndex(1);
+				if(Menu.isOpen())
+					Menu.clickIndex(1);
 			}
 			if(Players.getLocal().getInteracting() == null)
 				return false;
 		} 
 		while(Inventory.contains(VARS.COOKED_FISH_IDS[1])){
-			if(EAT_SALMON_ACTIONBAR.click(false)){
-				Timer t = new Timer(1000);
+			if(EAT_SALMON_ACTIONBAR != null && EAT_SALMON_ACTIONBAR.click(false)){
+				Timer t = new Timer(2000);
 				while(!Menu.isOpen() && t.isRunning()){
-					Task.sleep(200,300);
+					Task.sleep(50,75);
 				}
-				Menu.clickIndex(1);
+				if(Menu.isOpen())
+					Menu.clickIndex(1);
 			}
 			if(Players.getLocal().getInteracting() == null)
 				return false;
-		} 
+		}  
 		return true;
 	}
 }
